5a8505ec8094aef06cd69472237d3eaa379a25b1
VAMSI KARUTURI <krishnavamsikaruturi8@gmail.com>
2023-07-28 19:05:32 +0530

    test

--- src/main/java/com/example/kafkacodedecode/controller/RestControllerForKafkaMsg.java
+++ src/main/java/com/example/kafkacodedecode/controller/RestControllerForKafkaMsg.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/api")
 public class RestControllerForKafkaMsg {
 
+
     @Autowired
     Producer producer;
     @GetMapping("/producerMsg")
